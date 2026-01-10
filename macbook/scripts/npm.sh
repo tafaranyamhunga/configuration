aa6d3dc1b334ecf486b7c10f5fda4332c1c4360c
Tafara Nyamhunga <136159071+tafaranyamhunga@users.noreply.github.com>
2026-01-10 21:16:52 +0200

    Add script to install Node.js packages

---
+++ macbook/scripts/npm.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+: '
+Install NPM global packages
+
+	- npm: A tool to manage JavaScript packages and libraries
+	- Various JavaScript libraries for development
+
+'
+
+# Exit immediately if a command exits with a non-zero status, if an undefined variable is used, or if any command in a pipeline fails
+set -euo pipefail
+
+# cd to the $HOME directory
+cd "${HOME}" || exit
+
+# Function to print messages
+print_message() {
+	printf "\n========================================\n"
+	printf "%s\n" "$1"
+	printf "========================================\n\n"
+}
+
+# Install NPM global packages
+print_message "Installing NPM global packages ..."
+
+sleep 1
+
+npm install -g sass eslint prettier typescript react react-dom semistandard webpack webpack-cli
