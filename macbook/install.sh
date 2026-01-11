31097b4843d7e7bab6271e38f24e708c562af570
Tafara Nyamhunga <136159071+tafaranyamhunga@users.noreply.github.com>
2026-01-11 13:58:12 +0200

    Update comment for Homebrew installation script
    Clarified the comment regarding the Homebrew installation script to indicate that it also installs Homebrew packages.

--- macbook/install.sh
+++ macbook/install.sh
@@ -38,7 +38,7 @@ print_message "Proceeding with the installation ..."
 # Run the Rosetta installation script
 ./scripts/rosetta.sh
 
-# Run the Homebrew installation script
+# Run the Homebrew installation script and install Homebrew packages
 ./scripts/homebrew.sh
 
 # Run the VS Code extensions installation script
